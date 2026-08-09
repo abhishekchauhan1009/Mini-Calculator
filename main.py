6cc5409551e4a13ff20fa1ec0dffbf58ffc491bf
Abhishek Chauhan <au29182002@gmail.com>
2026-08-09 12:57:15 +0000

    Mini Calculator

---
+++ main.py
@@ -0,0 +1,23 @@
+a = int(input("Enter first number : "))
+
+b = int(input("Enter second number : "))
+
+op = (input("Enter Operator (HINT : +, -, *, /, % ) : "))
+
+if op == '+':
+    print(a+b)
+
+elif op == '-':
+    print(a-b)
+
+elif op == '*':
+    print(a*b)
+
+elif op == '/':
+    print(a/b)
+
+elif op == '%':
+    print(a%b)   
+
+else :
+    print("Invalid Operation")         
